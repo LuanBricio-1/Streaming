fb3e062963ceb8b830b42c5ad79999a2cfb53298
LuanBricio-1 <130419167+LuanBricio-1@users.noreply.github.com>
2024-12-29 02:32:15 +0000

    .

--- scripts/filmes.js
+++ scripts/filmes.js
@@ -1,13 +1,9 @@
+// papai noel
 
-// Os simpsons: Homer Papai Noel (1)
-var frame1 = document.getElementById('homerpapainoel');
-frame1.innerHTML += '<iframe src="https://embed.warezcdn.link/filme/tt34791030"'+ '" scrolling="no" frameborder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""></iframe>';
-
-// Arca de Noé (2)
-
-var frame2 = document.getElementById('arcadenoe');
-frame2.innerHTML += '<iframe src="https://embed.warezcdn.link/filme/tt8637446"'+ '" scrolling="no" frameborder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""></iframe>';
+var frame = document.getElementById('homerpapainoel');
+frame.innerHTML += '<iframe src="https://embed.warezcdn.link/filme/tt34791030"'+ '" scrolling="no" frameborder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""  height="500px"   width="500px"></iframe>';
 
 
+const section = document.getElementsByName('section')
 
-// Red Crescer é uma Fera (3)
+console.log(section)
